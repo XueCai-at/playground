66be3acf8c2c3d8334a19130306561342ac723b5
XueCai-at <xue.cai@airtable.com>
2021-01-06 18:55:39 -0800

    add a task

---
+++ tasks/check_service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const http = require('http')
+const options = {
+  hostname: 'xue-test-service-blue-green-alb-382889407.us-west-2.elb.amazonaws.com',
+  port: 80,
+  path: '/version',
+  method: 'GET'
+}
+
+console.log(`Running at time ${(new Date(Date.now())).toUTCString()} with code version ${process.env.CODE_VERSIO}`);
+
+const req = http.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
